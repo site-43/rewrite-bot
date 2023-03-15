06f89c029a0d2992667219068dbd9331a1c7233c
Saturna19-fr <saturna19.stream@outlook.fr>
2023-03-15 12:28:24 +0100

    updates

---
+++ main.py
@@ -0,0 +1,41 @@
+import interactions
+from interactions.ext import molter
+from flask import Flask
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+from asgiref.wsgi import WsgiToAsgi
+import asyncio
+import logging
+import time
+import os
+from configs import VERSION, OWNER_ID
+EXTENSIONS = [file.replace(".py", "") for file in os.listdir("exts") if file.endswith(".py")]
+
+# client = interactions.Client(token="") -> Bot Canarypy 
+client = interactions.Client(
+    token=os.getenv("TOKEN"),
+    intents=interactions.Intents.DEFAULT
+    | interactions.Intents.GUILD_MESSAGE_CONTENT
+    | interactions.Intents.GUILD_MEMBERS,
+    disable_sync=False,
+)
+app = Flask(__name__)
+config = Config()
+config.bind = ["0.0.0.0:80"]
+
+molter.setup(client, default_prefix=["s!","<"])
+[client.load(f"exts.{EXT}") for EXT in EXTENSIONS]
+
+
+@app.route('/')
+async def home():
+     return 'Running !'
+loop = asyncio.get_event_loop()
+
+task1 = loop.create_task((serve(WsgiToAsgi(app), config)))
+task2 = loop.create_task(client._ready())
+
+gathered = asyncio.gather(task1,task2)
+loop.run_until_complete(gathered)
+
+#client.start()
