6ada6de8567c46cd617fe7855dc1fa5f174d38c9
Saturna19-fr <saturna19.stream@outlook.fr>
2023-03-26 12:25:03 +0200

    status command (sh)

---
+++ exts/status.py
@@ -0,0 +1,15 @@
+import interactions
+
+class Extension(interactions.Extension):
+    def __init__(self, client):
+        self.client: interactions.Client = client
+
+    @interactions.extension_command(
+        name='status',
+        description='Obtenez le statut du bot.',
+    )
+    async def status(self, ctx:interactions.CommandContext):
+        await ctx.send("☑️")
+
+def setup(client):
+    Extension(client)
