7c6750c600cc383c87981f0f30c187e386fa584b
Saturna19-fr <saturna19.stream@outlook.fr>
2023-04-14 23:44:52 +0200

    test

--- exts/service.py
+++ exts/service.py
@@ -1,6 +1,7 @@
 import interactions
 from utils.embeds import new_embed, new_notify_embed, create_error_embed
 import time
+import datetime
 from utils.database import getData, addData, getCount, deleteData, updateData
 from utils.components import add_button
 compos = [
@@ -43,26 +44,40 @@ class Extension(interactions.Extension):
             embed = message.embeds[0]
             embed.fields[0].value = await getCount(collection="services")
             await message.edit(embeds=embed, components=compos)
+            await ctx.send(embeds=[new_notify_embed("Vous avez quitté votre service.")], ephemeral=True)
+        
 
-
-    
     @interactions.extension_command(
-        name="sendservice",
+        name="service",
+        description="Management du Service",
+        options=[
+            interactions.Option(type=interactions.OptionType.SUB_COMMAND, name="send", description="Envoyer l'embed dans le salon actuel."),
+            interactions.Option(type=interactions.OptionType.SUB_COMMAND, name="retreive", description="Récupère le temps de service dans la Database.", options=[interactions.Option(type=interactions.OptionType.USER, name="mod", description="Modérateur dont vous souhaitez obtenir le service.")])
+        ]
     )
-    async def sendservice(self, ctx: interactions.CommandContext):
-        channel = await ctx.get_channel()
-        await channel.send(
-            embeds=[
-                new_embed(
-                    title="Service",
-                    description="Prenez/arrêtez votre service avec le bouton çi-dessous.\nIl permets de vous octroyer le rôle En jeu, ainsi que de compter votre temps.",
-                    color=0xb39317,
-                    fields=[["Modérateurs en jeu", await getCount(collection="services"), False]]
-                )
-            ],
-            components=compos
-        )
-        await ctx.send(embeds=[new_notify_embed("Le système de service est désormais actif.")])
+    async def service(self, ctx: interactions.CommandContext, sub_command:str, mod:interactions.User = None):
+        if sub_command == "send":
+            channel = await ctx.get_channel()
+            await channel.send(
+                embeds=[
+                    new_embed(
+                        title="Service",
+                        description="Prenez/arrêtez votre service avec le bouton çi-dessous.\nIl permets de vous octroyer le rôle En jeu, ainsi que de compter votre temps.",
+                        color=0xb39317,
+                        fields=[["Modérateurs en jeu", await getCount(collection="services"), False]]
+                    )
+                ],
+                components=compos
+            )
+            await ctx.send(embeds=[new_notify_embed("Le système de service est désormais actif.")], ephemeral=True)
+        elif sub_command == "retreive":
+            data = await getData(collection="servicesSaved", searchValue={"_id": int(mod.id)})
+            if not data:
+                await ctx.send(embeds=[create_error_embed("L'utilisateur n'a pas de services enregistrés.")])
+                return
+            currentTime = datetime.timedelta(seconds=data["passedTime"])
+            print(currentTime)
+            await ctx.send(embeds=[new_notify_embed(f"{mod.mention} a passé {str(currentTime)} en service.")])
 
 def setup(client):
     Extension(client)
